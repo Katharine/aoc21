e717912a81f30b7df95347d6fdd399b1b1f86394
Katharine Berry <ktbry@google.com>
2021-12-11 21:21:32 -0800

    Day 12

---
+++ day12a.py
@@ -0,0 +1,35 @@
+# Copyright 2021 Google LLC.
+# SPDX-License-Identifier: Apache-2.0
+
+import string
+
+paths: list[tuple[str, str]] = []
+with open('day12.dat') as f:
+    for line in f:
+        paths.append(tuple(line.strip().split('-')))
+
+connections = {}
+
+for path in paths:
+    if path[0] not in connections:
+        connections[path[0]] = set()
+    connections[path[0]].add(path[1])
+    if path[1] not in connections:
+        connections[path[1]] = set()
+    connections[path[1]].add(path[0])
+
+routes = []
+queue = [['start', x] for x in connections['start']]
+
+while queue:
+    route = queue.pop(0)
+    if route[-1] == 'end':
+        routes.append(route)
+        continue
+    else:
+        cs = connections[route[-1]]
+        for c in cs:
+            if c[0] in string.ascii_uppercase or c not in route:
+                queue.append(route + [c])
+
+print(len(routes))
