ab000244174be20ed297139c359dc4ef194a9296
Katharine Berry <ktbry@google.com>
2021-12-15 22:01:16 -0800

    Day 16 cleanup

--- day16a.py
+++ day16a.py
@@ -2,11 +2,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import binascii
-import bitstring
+
+
+class Bitstream:
+    def __init__(self, hex: str):
+        self.data = ''.join(bin(x)[2:].zfill(8) for x in binascii.unhexlify(hex))
+        self.bitpos = 0
+
+    def read(self, bits: int) -> int:
+        v = int(self.data[self.bitpos:self.bitpos+bits], 2)
+        self.bitpos += bits
+        return v
+
 
 with open('day16.dat') as f:
-    data = binascii.unhexlify(f.readline().strip())
-    bs = bitstring.ConstBitStream(data)
+    bs = Bitstream(f.readline().strip())
 
 
 class Packet:
@@ -16,37 +26,36 @@ class Packet:
         self.subpackets = []
 
 
-def read_packet(data: bitstring.ConstBitStream, depth=0) -> (Packet, int):
-    start = data.bitpos
-    version = data.read('uint:3')
-    type_id = data.read('uint:3')
+def read_packet(data: Bitstream) -> Packet:
+    version = data.read(3)
+    type_id = data.read(3)
     if type_id == 4:
         value = 0
         while True:
-            is_last = data.read('uint:1')
-            chunk = data.read('uint:4')
+            is_last = data.read(1)
+            chunk = data.read(4)
             value = (value << 4) | chunk
             if not is_last:
                 break
-        return Packet(version, type_id), data.bitpos - start
-    length_type = data.read('uint:1')
+        return Packet(version, type_id)
+    length_type = data.read(1)
     if length_type == 0:
-        bits_to_read = data.read('uint:15')
+        bits_to_read = data.read(15)
         packet = Packet(version, type_id)
         inner_start = data.bitpos
         while (data.bitpos - inner_start) < bits_to_read:
-            p, r = read_packet(data, depth+1)
+            p = read_packet(data)
             packet.subpackets.append(p)
     else:
-        packets_to_read = data.read('uint:11')
+        packets_to_read = data.read(11)
         packet = Packet(version, type_id)
         for i in range(packets_to_read):
-            p, r = read_packet(data, depth+1)
+            p = read_packet(data)
             packet.subpackets.append(p)
-    return packet, data.bitpos - start
+    return packet
 
 
-packet, read = read_packet(bs)
+packet = read_packet(bs)
 
 def sum_versions(packet: Packet) -> int:
     return packet.version + sum(map(sum_versions, packet.subpackets))
